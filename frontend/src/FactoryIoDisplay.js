3cfeb6f6dbd80501c117eb7504cf7e0d9e7c82f4
Myles <Myleshosken@hotmail.com>
2023-04-29 10:31:30 +1200

    Updating Database test values

--- frontend/src/FactoryIoDisplay.js
+++ frontend/src/FactoryIoDisplay.js
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react";
 import database from "./firebaseConfig";
-import { ref, onValue, off } from "firebase/database";
+import { set, ref, onValue, off } from "firebase/database";
 const FactoryIoDisplay = () => {
     const [machineData, setMachineData] = useState(null);
 
     useEffect(() => {
-        const machineRef = ref(database, "factory_io/machines/machine_1");
+        const machineRef = ref(database, "factory_io/machines/elevatorAdvanced");
 
         const handleDataChange = (snapshot) => {
             setMachineData(snapshot.val());
@@ -19,14 +19,23 @@ const FactoryIoDisplay = () => {
 
     }, []);
 
+
+    function testClick () {
+      const machineRef = ref(database, "factory_io/machines/elevatorAdvanced/coils/Conveyor0/value");
+      set(machineRef, 1);
+    }
+
     return  (
         <div>
         <h1>Machine Data</h1>
+        <button className="button" onClick={testClick}></button>
         {machineData ? (
           <>
-            <p>Temperature: {machineData.temperature}</p>
-            <p>Humidity: {machineData.humidity}</p>
-            <p>Timestamp: {machineData.timestamp}</p>
+            <p>elevatorAdvanced</p>
+            <p>{machineData.coils.Conveyor0.name} : {machineData.coils.Conveyor0.value}</p>
+            <p>{machineData.coils.Conveyor1.name} : {machineData.coils.Conveyor1.value}</p>
+            <p>{machineData.coils.Conveyor2.name} : {machineData.coils.Conveyor2.value}</p>
+            <p>{machineData.coils.Conveyor3.name} : {machineData.coils.Conveyor3.value}</p>
           </>
         ) : (
           <p>Loading data...</p>
