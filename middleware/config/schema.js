def2a13d2de33490cdc37bb58c7c0f81a3408199
Joshua Ladowsky <33144900+Kohjen@users.noreply.github.com>
2023-05-02 11:20:49 +1200

    Revert "Merge pull request #2 from emsoftaut/middleware-init"
    This reverts commit bf765ca560e648bede202af3827f4567eae1450e, reversing
    changes made to 3cfeb6f6dbd80501c117eb7504cf7e0d9e7c82f4.

--- middleware/config/schema.js
+++
@@ -1,7 +0,0 @@
-const dbStructure = {
-    database: "factory_io",
-    table: "machines",
-    getPath: function() {return this.database + "/" + this.table + "/";}
-}
-
-module.exports = dbStructure;
