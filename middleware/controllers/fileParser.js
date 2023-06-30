32d523e663cfac0c40ed11e75aeb93d95cfc8f07
Kohjen <33144900+Kohjen@users.noreply.github.com>
2023-06-30 20:28:29 +1200

    1.0 update
    adding in file saving as well as formatting for object.
    reformatted both save files as description now needs to be the name of the model
    added new JSONs generated by script
    TODO: refactor

--- middleware/controllers/fileParser.js
+++ middleware/controllers/fileParser.js
@@ -1,41 +1,77 @@
+const { ok } = require("assert");
 const fs = require("fs");
 const savePath = "../factoryIO";
 const convert = require("xml-js");
-let keyList = {};
-let output = {};
-fs.readdir(savePath, "buffer", (err, files) => {
-	if (err) console.log(err);
-	else {
-		files.forEach((file) =>
-			fs.readFile(savePath + "/" + file, "utf8", (err, content) => {
-				if (err) console.log(err);
-				else saveFile = convert.xml2json(content, { compact: true, spaces: 4 });
-				saveData = JSON.parse(saveFile).FactoryIO;
-				driverDetails = saveData.Drivers.ModbusTCPServer;
-				objectDetails = saveData.Object;
-				for (let key in driverDetails) {
-					if (key.startsWith("BitInput")) {
-						let bitNum = key.replace("BitInput", ""); // Extracting the bit number from the key
-						let pointIOKey = driverDetails[key]._attributes.PointIOKey;
-						keyList[pointIOKey] = "S" + bitNum; // Storing the PointIOKey in the new object
-					} else if (key.startsWith("BitOutput")) {
-						let bitNum = key.replace("BitOutput", ""); // Extracting the bit number from the key
-						let pointIOKey = driverDetails[key]._attributes.PointIOKey;
-						(keyList["K" + bitNum] = pointIOKey), "test"; // Storing the PointIOKey in the new object
-					}
-				}
 
-				for (let objKey in objectDetails) {
-					if (checkNested(objectDetails[objKey], "OperatingMode", "GroupIO", "BinaryOutput", "_attributes", "Key")) {
-						console.log(objectDetails[objKey]);
-					} else if (checkNested(objectDetails[objKey], "OperatingMode", "GroupIO", "BinaryOutput", "_attributes", "Key")) {
-						console.log(objectDetails[objKey]);
+generateJSONFiles();
+
+function generateJSONFiles() {
+	fs.readdir(savePath, "buffer", (err, files) => {
+		if (err) console.log(err);
+		else {
+			files.forEach((file) =>
+				fs.readFile(savePath + "/" + file, "utf8", (err, content) => {
+					let output = { name: "", sensors: {}, coils: {} };
+					if (err) console.log(err);
+					else saveFile = convert.xml2json(content, { compact: true, spaces: 4 });
+					saveData = JSON.parse(saveFile).FactoryIO;
+					output.name = saveData.Description._attributes.Data;
+					// console.log(output);
+					driverDetails = saveData.Drivers.ModbusTCPServer;
+					objectDetails = saveData.Object;
+					let driverList = {};
+					for (let key in driverDetails) {
+						if (key.startsWith("BitInput")) {
+							let bitNum = key.replace("BitInput", ""); // Extracting the bit number from the key
+							let pointIOKey = driverDetails[key]._attributes.PointIOKey;
+							driverList[pointIOKey] = { IOType: "input", bitnum: bitNum, IOKey: pointIOKey }; // Storing the PointIOKey in the new object
+						} else if (key.startsWith("BitOutput")) {
+							let bitNum = key.replace("BitOutput", ""); // Extracting the bit number from the key
+							let pointIOKey = driverDetails[key]._attributes.PointIOKey;
+							driverList[pointIOKey] = { IOType: "output", bitnum: bitNum, IOKey: pointIOKey }; // Storing the PointIOKey in the new object
+						}
 					}
-				}
-			})
-		);
-	}
-});
+					let objList = {};
+					for (let objKey in objectDetails) {
+						if (checkNested(objectDetails[objKey], "OperatingMode", "GroupIO", "BinaryOutput", "_attributes", "Key")) {
+							objList[objectDetails[objKey].OperatingMode.GroupIO.BinaryOutput._attributes.Key] = {
+								_attributes: objectDetails[objKey].OperatingMode.GroupIO.BinaryOutput._attributes,
+								operatingValue: objectDetails[objKey].ComponentIO._attributes.CurrentOperatingMode,
+							};
+						} else if (checkNested(objectDetails[objKey], "OperatingMode", "GroupIO", "BinaryInput", "_attributes", "Key")) {
+							objList[objectDetails[objKey].OperatingMode.GroupIO.BinaryInput._attributes.Key] = {
+								_attributes: objectDetails[objKey].OperatingMode.GroupIO.BinaryInput._attributes,
+								operatingValue: objectDetails[objKey].ComponentIO._attributes.CurrentOperatingMode,
+							};
+						}
+					}
+					objectKeys = Object.keys(objList);
+					driverKeys = Object.keys(driverList);
+					for (let oKey in objectKeys) {
+						for (let dKey in driverKeys) {
+							if (driverKeys[dKey] === objectKeys[oKey]) {
+								if (driverList[driverKeys[dKey]].IOType == "input") {
+									output.sensors["I" + driverList[driverKeys[dKey]].bitnum] = {
+										name: objList[objectKeys[oKey]]._attributes.Name,
+										value: objList[objectKeys[oKey]].operatingValue,
+										register: driverList[driverKeys[dKey]].bitnum,
+									};
+								} else {
+									output.coils["o" + driverList[driverKeys[dKey]].bitnum] = {
+										name: objList[objectKeys[oKey]]._attributes.Name,
+										value: objList[objectKeys[oKey]].operatingValue,
+										register: driverList[driverKeys[dKey]].bitnum,
+									};
+								}
+							}
+						}
+					}
+					fs.writeFileSync("./models/middleware/" + output.name + ".JSON", JSON.stringify(output));
+				})
+			);
+		}
+	});
+}
 
 function checkNested(obj /*, level1, level2, ... levelN*/) {
 	var args = Array.prototype.slice.call(arguments, 1);
