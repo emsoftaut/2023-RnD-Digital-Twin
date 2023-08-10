7f15024964247755672e4efeace04320304b6b60
janenotjung-hue <107402425+janenotjung-hue@users.noreply.github.com>
2023-08-10 16:25:05 +1200

    changed dropdown
    changed dropdown to follow mui standard

--- frontend/src/components/DropdownProfile.js
+++ frontend/src/components/DropdownProfile.js
@@ -1,11 +1,21 @@
 import React, { useContext } from "react";
-import styles from "./style.module.css";
-import {FaCog, FaInfoCircle, FaSignOutAlt, FaMoon} from "react-icons/fa";
+//import {FaInfoCircle, FaSignOutAlt} from "react-icons/fa";
+import { styled } from '@mui/material/styles';
+import { Box, Grid, Paper, Avatar, Button, Typography, useTheme } from "@mui/material";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import AuthContext from "./AuthContext";
 
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundImage: "none",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  boxShadow: "none",
+  color: theme.palette.text.secondary,
+}));
 
 const DropdownProfile = () => {
-
+  const theme = useTheme();
     const { handleLogout } = useContext(AuthContext);
  
     const handleLogoutClick = () =>  {
@@ -13,19 +23,41 @@ const DropdownProfile = () => {
     }
       
         return (
-            <div className={styles.dropdownprofile}>
-              <ul>
-                <li onClick={handleLogoutClick}>
-                  <FaSignOutAlt style={{ verticalAlign: "top", display: "inline-block" }} /> Logout
-                </li>
-                <hr className="solid" style={{ borderTop: "1px solid gray" }} />
-                <a href="/">
-                  <li>
-                    <FaInfoCircle style={{ verticalAlign: "top", display: "inline-block" }} /> Help
-                  </li>
-                </a>
-              </ul>
-            </div>
+            <Box sx={{ flexGrow: 1, background: theme.palette.background.default}}>
+        <Grid
+          container
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+          paddingX={5}
+          paddingY={2}
+        >
+
+                <Grid item xs>
+                <Item><Avatar sx={{ width: 56, height: 56 }} alt="Jane Jung"/></Item>
+                </Grid>
+                <Grid item xs>
+                <Item><Typography variant="h6">Jane Jung</Typography></Item>
+              
+                </Grid>
+                <Grid item xs>
+                <Item><Typography variant="p">janejung@email.com</Typography></Item>
+              
+                </Grid>
+                <Grid item xs>
+                <Item><Button>Settings</Button></Item>
+              
+                </Grid>
+                <Grid item xs>
+                <Item><Button>Help</Button></Item>
+              
+                </Grid>
+                <Grid item xs>
+                <Item><Button onClick={handleLogoutClick}>Logout</Button></Item>
+            
+                </Grid>
+              </Grid>
+              </Box>
           );
 }
 
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,21 +1,19 @@
 import React, { useContext, useState, useEffect } from "react";
 import { ColorModeContext } from "../theme";
-import { Box, IconButton, useTheme } from "@mui/material";
+import { Box, IconButton, Popover, useTheme, Button } from "@mui/material";
+import PopupState, { bindTrigger, bindPopover } from 'material-ui-popup-state';
 import { authInstance } from "../firebaseConfig";
 import { getFunctions, httpsCallable } from "firebase/functions";
-import { Button } from "@mui/material";
 import { Link } from 'react-router-dom';
 import { onAuthStateChanged } from "firebase/auth";
 import DropdownProfile from "./DropdownProfile";
 import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
-import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
 
 const Navbar = () => {
     const theme = useTheme();
     const colorMode = useContext(ColorModeContext);
-    const [openProfile, setOpenProfile] = useState(false);
     const [isAdmin, setIsAdmin] = useState(false);
     const [isAuthenticated, setIsAuthenticated] = useState(false);
     const auth = authInstance;
@@ -43,18 +41,14 @@ const Navbar = () => {
     }, [auth, functions]);
 
 
-    const handleProfileClick = () => {
-        setOpenProfile((prev) => !prev); // Toggle the openProfile state
-    };
-
 
     return (
         <Box sx={{ display: "flex", justifyContent: "space-between", padding: 2, backgroundColor: (theme.palette.mode === "dark") ? theme.palette.grey[900] : theme.palette.primary.main }}>
-            <Box display="flex">
+            <Box display="flex" height={50}>
                 {theme.palette.mode === 'dark' ? (
-                    <img src="./logo-dark.png" alt="logo" width={50} style={{ marginTop: 5 + 'px', marginBottom: 5 + 'px' }} />
+                    <img src="./logo-dark.png" alt="logo" style={{ marginTop: 5 + 'px', marginBottom: 5 + 'px', objectFit: "cover" }} />
                 ) : (
-                    <img src="./logo-light.png" alt="logo" width={50} style={{ marginTop: 5 + 'px', marginBottom: 5 + 'px' }} />
+                    <img src="./logo-light.png" alt="logo" style={{ marginTop: 5 + 'px', marginBottom: 5 + 'px', objectFit: "cover" }} />
                 )}
             </Box>
             <Box display="flex">
@@ -62,6 +56,7 @@ const Navbar = () => {
                     <Link to="/admin">
                         <Button sx={{
                             height: 50,
+                            marginRight: "10px",
                         }}
                             variant="contained"
                             type="submit">
@@ -78,17 +73,30 @@ const Navbar = () => {
                     )}
                 </IconButton>
                 {isAuthenticated && (
-                    <>
-                        <IconButton aria-label="Settings">
-                            <SettingsOutlinedIcon />
-                        </IconButton>
-                        <IconButton aria-label="Profile" onClick={handleProfileClick}>
-                            <PersonOutlinedIcon />
-                        </IconButton>
-                        {
-                            openProfile && (<DropdownProfile />)
-                        }
-                    </>
+                    <PopupState variant="popover" popupId="profilePopup">
+                    {(popupState) => (
+                        <>
+                            <IconButton aria-label="Profile" {...bindTrigger(popupState)}>
+                                <PersonOutlinedIcon />
+                            </IconButton>
+                            <Popover 
+                            {...bindPopover(popupState)} 
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'right',
+                                }}
+                                transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}>
+                                <DropdownProfile/>
+                            </Popover>
+                        </>
+                    )}
+                    
+
+                    </PopupState>
+                   
                 )}
             </Box>
         </Box>
