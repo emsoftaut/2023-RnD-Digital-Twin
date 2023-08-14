92d5630e47617e715c9e4051b1accb6ba2906220
janenotjung-hue <107402425+janenotjung-hue@users.noreply.github.com>
2023-08-14 13:46:46 +1200

    Added Popup
    added Harshil's popup

---
+++ frontend/src/components/JobPopup.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import "./JobPopup.css";
+
+const JobPopup = ({ onClose, machineName }) => {
+	const [jobsQueueNum, setjobsQueue] = useState("");
+	const machineID = machineName;
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		onClose(jobsQueueNum, machineID);
+	};
+
+	const handlevalueChange = (event) => {
+		setjobsQueue(event.target.value);
+	};
+
+	return (
+		<div className="popup">
+			<div className="popup-inner">
+				<h1>Job Request</h1>
+				<form onSubmit={handleSubmit}>
+					<label>
+						Machine ID: {machineName}
+						<br />
+						Number of Boxes:
+						<input type="number" value={jobsQueueNum} onChange={handlevalueChange} />
+					</label>
+					<button type="submit">Submit</button>
+				</form>
+				<button className="popup-cb">Close</button>
+			</div>
+		</div>
+	);
+};
+
+export default JobPopup;
