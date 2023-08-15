dfa16286b20f70671ef7e1705424a4e899b7aea1
dtm3080 <harshilpatel3010@gmail.com>
2023-08-15 12:01:41 +1200

    updating.

--- frontend/src/components/JobPopup.js
+++ frontend/src/components/JobPopup.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { TextField, Button, useTheme, Box } from "@mui/material";
+import { Button, useTheme, Box } from "@mui/material";
 import "./JobPopup.css";
 
 const JobPopup = ({ onClick, machineName, onClose }) => {
@@ -18,34 +18,92 @@ const JobPopup = ({ onClick, machineName, onClose }) => {
 	};
 
 	const handleClose = () => {
-		onClose(null);
+		onClose(false);
 	};
 
 	return (
-		<div className="popup">
-			<div className="popup-inner">
-			<button className="popup-cb" onClick={() => onClose(false)}>
-					Close
-				</button>
-				<h1>Job Request</h1>
+		<Box
+			sx={{
+				position: "fixed",
+				top: 0,
+				left: 0,
+				width: "100%",
+				height: "100%",
+				display: "flex",
+				justifyContent: "center",
+				alignItems: "center",
+				zIndex: 10000,
+				backgroundColor: "rgba(0, 0, 0, 0.5)",
+			}}
+		>
+			<Box
+				sx={{
+					padding: "20px",
+					borderRadius: "8px",
+					boxShadow: "0px 2px 10px rgba(0, 0, 0, 0.3)",
+					width: "500px",
+					color: "black",
+					//backgroundColor: "white",
+					backgroundColor: theme.background,
+				}}
+			>
 				<form onSubmit={handleSubmit}>
-					<label>
-						Machine ID: {machineName}
-						<br />
+					<h1 sx={{ marginBottom: 16 }}>Order Form</h1>
+					<h4>Machine ID: {machineName}</h4>
+					<label sx={{ display: "block", marginButtom: "8px" }}>
 						Number of Boxes:
-						<input className="form-input" type="number" value={jobsQueueNum} onChange={handlevalueChange} />
+						<br />
+						<input sx={{ width: "100%", padding: "8px", border: "1px solid #a1a1a1", borderRadius: "4px" }} type="number" value={jobsQueueNum} onChange={handlevalueChange} />
 					</label>
-					<div className="form-button-group">
-						<button className="form-submit-button" type="submit">
+					<Box
+						sx={{
+							display: "flex",
+							justifyContent: "space-evenly",
+							alignItems: "center",
+							marginTop: "16px",
+						}}
+					>
+						<Button sx={{ color: "white", border: "none", padding: "8px 16px", borderRadius: "4px", cursor: "pointer" }} variant="contained" type="submit">
 							Submit
-						</button>
-						<button className="close-button" onClick={handleClose}>
+						</Button>
+						<Button
+							sx={{
+								color: "white",
+								border: "none",
+								padding: "8px 16px",
+								borderRadius: "4px",
+								cursor: "pointer",
+							}}
+							variant="contained"
+							onClick={handleClose}
+						>
 							Close
-						</button>
-					</div>
+						</Button>
+					</Box>
 				</form>
-			</div>
-		</div>
+			</Box>
+		</Box>
+
+		// <div className="popup">
+		// 	<div className="popup-content">
+		// 		<form className="form" onSubmit={handleSubmit}>
+		// 			<h1 className="popup-title">Order Form</h1>
+		// 			<h4>Machine ID: {machineName}</h4>
+		// 			<label className="form-label">
+		// 				Number of Boxes:
+		// 				<input className="form-input" type="number" value={jobsQueueNum} onChange={handlevalueChange} />
+		// 			</label>
+		// 			<div className="form-button-group">
+		// 				<button className="form-submit-button" type="submit">
+		// 					Submit
+		// 				</button>
+		// 				<button className="close-button" onClick={handleClose}>
+		// 					Close
+		// 				</button>
+		// 			</div>
+		// 		</form>
+		// 	</div>
+		// </div>
 	);
 };
 
