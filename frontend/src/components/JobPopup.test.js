a1ffed30c783fcda830f668e4aa9e86d7a5c2353
dtm3080 <harshilpatel3010@gmail.com>
2023-08-22 11:30:16 +1200

    testing 3 out of 4 test pass

---
+++ frontend/src/components/JobPopup.test.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import JobPopup from "./JobPopup";
+
+describe("JobPopup Component", () => {
+	it("renders the machine ID correctly", () => {
+		render(<JobPopup onClose={() => {}} onClick={() => {}} machineName="Machine123" />);
+
+		const machineIdElement = screen.getByText("Machine ID: Machine123");
+		expect(machineIdElement).toBeInTheDocument();
+	});
+
+	it("submits the form with the given number of boxes", () => {
+		const mockOnClick = jest.fn();
+		render(<JobPopup onClose={() => {}} onClick={mockOnClick} machineName="Machine123" />);
+
+		const inputElement = screen.getByLabelText("Number of Boxes:");
+		userEvent.type(inputElement, "5");
+
+		const submitButton = screen.getByText("SUBMIT");
+		userEvent.click(submitButton);
+
+		expect(mockOnClick).toHaveBeenCalledWith(5);
+	});
+
+	it("closes the popup when the close button is clicked", async () => {
+		const mockOnClose = jest.fn();
+		render(<JobPopup onClose={mockOnClose} onClick={() => {}} machineName="Machine123" />);
+
+		const closeButton = screen.getByText("CLOSE");
+		userEvent.click(closeButton);
+
+		// Wait for the component to re-render after state update
+		await waitFor(() => {
+			expect(mockOnClose).toHaveBeenCalledWith(false);
+		});
+	});
+
+	it("does not call onClick if the number of boxes is not provided", () => {
+		const mockOnClick = jest.fn();
+		render(<JobPopup onClose={() => {}} onClick={mockOnClick} machineName="Machine123" />);
+
+		const submitButton = screen.getByText("SUBMIT");
+		userEvent.click(submitButton);
+
+		expect(mockOnClick).not.toHaveBeenCalled();
+	});
+});
