368a21d024fbf39495a5b0050b579c03057718a0
Myles <Myleshosken@hotmail.com>
2023-08-21 13:09:47 +1200

    Testing Login

--- frontend/src/__test__/Login.test.js
+++ frontend/src/__test__/Login.test.js
@@ -1,8 +1,34 @@
-import { render } from "@testing-library/react";
-import Login from "../components/Login";
+import { render, fireEvent } from '@testing-library/react';
+import AuthContext from '../components/AuthContext';
+import Login from '../components/Login';
+import { BrowserRouter } from 'react-router-dom';
 
-it("Renders without crashing", () => {
-	render(<Login />);
+const renderWithAuthContext = (component) => {
+  const mockContextValue = {
+    setUserManually: jest.fn(),
+  };
+
+  return render(
+    <AuthContext.Provider value={mockContextValue}>
+      <BrowserRouter>
+        {component}
+      </BrowserRouter>
+    </AuthContext.Provider>
+  );
+};
+
+describe('Login Component', () => {
+  it('Renders without crashing', () => {
+    const { container } = renderWithAuthContext(<Login />);
+    expect(container).toBeInTheDocument();
+  });
+
+  it('Handles email input correctly', () => {
+    const { getByLabelText } = renderWithAuthContext(<Login />);
+    const emailInput = getByLabelText(/Email/i);
+
+    fireEvent.change(emailInput, { target: { value: 'test@example.com' } });
+
+    expect(emailInput.value).toBe('test@example.com');
+  });
 });
-
-it("Accepts Correct Login", () => {});
