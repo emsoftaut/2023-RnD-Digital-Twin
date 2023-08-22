d3d02d03ac312ac494a90396d371c25c822629d5
janenotjung <janejung1221@gmail.com>
2023-08-22 18:45:23 +1200

    Reformatted AllMachineTable
    removed functions meant for firebase

--- frontend/src/components/machineComponents/AllMachineTable.js
+++ frontend/src/components/machineComponents/AllMachineTable.js
@@ -1,64 +1,13 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Box, Button, Table, TableBody, TableHead, TableRow, TableCell, LinearProgress, Typography, IconButton } from "@mui/material";
-import { appDb } from "../../firebaseConfig";
-import { ref, get, set, onValue, off } from "firebase/database";
 import { Link } from "react-router-dom";
 import JobPopup from "../JobPopup"
 import PauseIcon from '@mui/icons-material/Pause';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import StopIcon from '@mui/icons-material/Stop';
-import { grey } from "@mui/material/colors";
+import { toggleMachine, useMachineData } from "../../data/FireBaseData";
 
-async function toggleMachine(machID) {
-	// Get the reference to the database path where "running" variable is stored
-	const databasePath = `factory_io/data/${machID}/coils/running`;
-	const databaseRef = ref(appDb, databasePath);
-
-	try {
-		// Read the current status from the database
-		const snapshot = await get(databaseRef);
-		const currentStatus = snapshot.val();
-
-		// Calculate the new status (toggle the status) and update the database
-		const newStatus = !currentStatus;
-		set(databaseRef, newStatus)
-			.then(() => {
-				console.log("Machine status updated successfully!");
-			})
-			.catch((error) => {
-				console.error("Error updating machine status:", error);
-			});
-	} catch (error) {
-		console.error("Error reading machine status:", error);
-	}
-}
-
-async function setJQMachine(machID, JQ) {
-	// Get the reference to the database path where "jobsQueued" variable is stored
-	const databasePath = `factory_io/data/${machID}/coils/jobsQueued`;
-	const databaseRef = ref(appDb, databasePath);
-
-	try {
-		// Read the current status from the database
-		const snapshot = await get(databaseRef);
-		const currentStatus = snapshot.val();
-
-		// Calculate the new status (toggle the status) and update the database
-		if (JQ !== currentStatus) {
-			set(databaseRef, JQ)
-				.then(() => {
-					console.log("Machine status updated successfully!");
-				})
-				.catch((error) => {
-					console.error("Error updating machine status:", error);
-				});
-		}
-	} catch (error) {
-		console.error("Error reading machine status:", error);
-	}
-}
-
-const PopUpButton = ({ machID, showpop }) => {
+export const PopUpButton = ({ machID, showpop }) => {
 	const [showPopup, setshowPopup] = useState(showpop);
 
 	const handlePopupClick = (jQ) => {
@@ -82,7 +31,7 @@ const PopUpButton = ({ machID, showpop }) => {
 	);
 };
 
-const MachineButton = (props) => {
+export const MachineButton = (props) => {
 	let { machID, running, method, jQ } = props;
 	let innerIcon = method === "toggle" ? (running ? <PauseIcon /> : <PlayArrowIcon/>) : <StopIcon/>;
 	let innerText = method === "toggle" ? (running ? "Pause" : "Resume") : "Stop";
@@ -91,7 +40,9 @@ const MachineButton = (props) => {
 	};
 
 	return (
-		<Button startIcon={innerIcon} 
+		<Button 
+		id= "machineButton"
+		startIcon={innerIcon} 
 		disableElevation 
 		variant="contained" 
 		color="grey" 
@@ -102,7 +53,7 @@ const MachineButton = (props) => {
 	);
 };
 
-const ProgressBar = ({ done, queued }) => {
+export const ProgressBar = ({ done, queued }) => {
 	let convertValue = (done / queued) * 100;
 	return (
 		<Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
@@ -118,39 +69,9 @@ const ProgressBar = ({ done, queued }) => {
 	);
 };
 
-function validateMachineData(machine) {
-	const requiredKeys = ["machineID", "coils", "lastModified", "sensors"];
-	return requiredKeys.every((key) => machine.hasOwnProperty(key) && machine[key] !== null && Object.keys(machine[key]).length !== 0);
-}
-
 const AllMachineTable = () => {
-	const [machineData, setMachineData] = useState([]);
-	const [error, setError] = useState(null);
-
+	const { machineData, error } = useMachineData();
 	const [showPopup] = useState(false);
-
-	useEffect(() => {
-		const machineRef = ref(appDb, "factory_io/data");
-
-		const handleDataChange = (snapshot) => {
-			const data = snapshot.val();
-			if (data) {
-				const dataArray = Object.values(data)
-					.filter(validateMachineData)
-					.map((machine) => ({
-						machID: machine.machineID,
-						...machine,
-					}));
-				setMachineData(dataArray);
-			}
-		};
-
-		onValue(machineRef, handleDataChange);
-
-		return () => {
-			off(machineRef, "value", handleDataChange);
-		};
-	}, []);
 
 	if (error) {
 		setError(error);
