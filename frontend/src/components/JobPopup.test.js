a0508fd5d27504697ea95952a4006ae1cb3e94b6
dtm3080 <harshilpatel3010@gmail.com>
2023-08-28 14:33:52 +1200

    wrote some unit test for job popup

--- frontend/src/components/JobPopup.test.js
+++ frontend/src/components/JobPopup.test.js
@@ -1,49 +1,74 @@
 import React from "react";
-import { render, screen, waitFor } from "@testing-library/react";
+import { render, fireEvent, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import JobPopup from "./PopUps/JobPopup";
-import WarningPopUp from "./PopUps/WarningPopUp";
+import JobPopup from "./PopUps/JobPopup"; // Update the import path
+import AllMachineTable from "./machineComponents/AllMachineTable"; // Update the import path
 
 describe("JobPopup Component", () => {
-	it("renders the machine ID correctly", () => {
-		render(<JobPopup onClose={() => {}} onClick={() => {}} machineName="Machine123" />);
+	it("triggers the sumbit button when the SUBMIT button is pressed", () => {
+		const onClickMock = jest.fn();
 
-		const machineIdElement = screen.getByText("Machine ID: Machine123");
-		expect(machineIdElement).toBeInTheDocument();
-	});
-	
-	it("submits the form with the given number of boxes", () => {
-		const mockOnClick = jest.fn();
-		render(<JobPopup onClose={() => {}} onClick={mockOnClick} machineName="Machine123" />);
-
-		const inputElement = screen.getByLabelText("Number of Boxes:");
-		userEvent.type(inputElement, "5");
+		render(<JobPopup onClick={onClickMock} machineName="Machine123" onClose={() => {}} />);
 
 		const submitButton = screen.getByText("SUBMIT");
-		userEvent.click(submitButton);
+		const input = screen.getByLabelText("Number of Boxes:");
 
-		expect(mockOnClick).toHaveBeenCalledWith(5);
+		userEvent.type(input, "5");
+		fireEvent.click(submitButton);
+
+		expect(onClickMock).toHaveBeenCalledWith("5");
 	});
 
-	it("closes the popup when the close button is clicked", async () => {
-		const mockOnClose = jest.fn();
-		render(<JobPopup onClose={mockOnClose} onClick={() => {}} machineName="Machine123" />);
+	it("resets input value when the CLOSE button is clicked", () => {
+		render(<JobPopup onClick={() => {}} machineName="Machine123" onClose={() => {}} />);
+
+		const closeButton = screen.getByText("CLOSE");
+		const input = screen.getByLabelText("Number of Boxes:");
+
+		userEvent.type(input, "5");
+		fireEvent.click(closeButton);
+
+		expect(input.value).toBe("0");
+	});
+
+	it("accepts a number input and rejects other data types", () => {
+		render(<JobPopup onClick={() => {}} machineName="Machine123" onClose={() => {}} />);
+
+		const input = screen.getByLabelText("Number of Boxes:");
+
+		userEvent.type(input, "5");
+		expect(input.value).toBe("5");
+
+		userEvent.clear(input);
+		userEvent.type(input, "abc");
+		expect(input.value).toBe(""); // Input should be empty after invalid input
+
+		userEvent.clear(input);
+		userEvent.type(input, "123abc");
+		expect(input.value).toBe("123"); // Only numeric part of input should be accepted
+	});
+
+	it("closes the popup when the close button is pressed", () => {
+		const onCloseMock = jest.fn();
+
+		render(<JobPopup onClick={() => {}} machineName="Machine123" onClose={onCloseMock} />);
 
 		const closeButton = screen.getByText("CLOSE");
 		userEvent.click(closeButton);
 
-		await waitFor(() => {
-			expect(mockOnClose).toHaveBeenCalledWith(false);
-		});
+		expect(onCloseMock).toHaveBeenCalledWith(false);
 	});
+});
 
-	it("does not call onClick if the number of boxes is not provided", () => {
-		const mockOnClick = jest.fn();
-		render(<JobPopup onClose={() => {}} onClick={mockOnClick} machineName="Machine123" />);
+describe("AllMachineTable Component", () => {
+	it("renders the job popup when the button is pressed", () => {
+		render(<AllMachineTable />);
 
-		const submitButton = screen.getByText("SUBMIT");
-		userEvent.click(submitButton);
+		const jobRequestButton = screen.getByText("Order");
 
-		expect(mockOnClick).not.toHaveBeenCalled();
+		userEvent.click(jobRequestButton);
+
+		// Verify that the job popup is rendered
+		expect(screen.getByText("Jobs Request Form")).toBeInTheDocument();
 	});
 });
