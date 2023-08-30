1763a20d86264b99aa2c060d9f4bee057131de1b
janenotjung <janejung1221@gmail.com>
2023-08-30 18:06:21 +1200

    toggle sidebar works (check iconbutton padding)

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,17 +1,18 @@
-import React, { useEffect, useState } from "react";
-import { useTheme, Box, Button, List, Collapse, Drawer, ListItemIcon, ListItemButton, ListItemText } from "@mui/material";
+import React, { useState } from "react";
+import { useTheme, Box, List, Collapse, ListItemIcon, ListItemButton, ListItemText, IconButton } from "@mui/material";
 import { Link } from "react-router-dom";
+import { useMachineData } from '../data/FireBaseData';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import PrecisionManufacturingIcon from '@mui/icons-material/PrecisionManufacturing';
+import DashboardIcon from '@mui/icons-material/Dashboard';
 import ExpandLess from '@mui/icons-material/ExpandLess';
 import ExpandMore from '@mui/icons-material/ExpandMore';
-import DashboardIcon from '@mui/icons-material/Dashboard';
-import { useMachineData } from '../data/FireBaseData';
-
 
 export const defaultDrawerWidth = 300;
 const minDrawerWidth = 50;
 const maxDrawerWidth = 300;
-
 
 const Sidebar = () => {
     const { machineData, error } = useMachineData();
@@ -41,7 +42,7 @@ const Sidebar = () => {
                 component={Link} to={to}
                 sx={{ paddingY: "15px" }}>
                 <ListItemIcon >{icon}</ListItemIcon>
-                {(drawerWidth === 300 ? <ListItemText primary={text} /> : null)}
+                <ListItemText primary={text} />
             </ListItemButton>
         );
     };
@@ -49,20 +50,33 @@ const Sidebar = () => {
     return (
         <Box width={drawerWidth} backgroundColor={(theme.mode === "dark" ? '#121212' : "#fff")}>
             <List component="nav" sx={{ paddingX: "5px" }}>
-                <ListItemButton sx={{ paddingY: "15px" }} icon={<DashboardIcon />} onClick={handleToggle} />
-                <ListItem name="index" text="All Machines" to="/" icon={<DashboardIcon />} />
-                <ListItemButton sx={{ paddingY: "15px" }} onClick={openSubList}>
-                    <ListItemIcon><PrecisionManufacturingIcon /></ListItemIcon>
-                    {(drawerWidth === 300 ? <ListItemText primary="Machine Details" /> : null)}{innerOpen ? <ExpandLess /> : <ExpandMore />}
-                </ListItemButton>
+                {drawerWidth === 300 ? <>
+                    <Box display={'flex'} flexDirection={'row-reverse'}>
+                        <IconButton sx={{ alignSelf: "right", paddingY: "15px"}} onClick={handleToggle}>
+                            <ChevronLeftIcon />
+                        </IconButton>
+                    </Box>
+                    <ListItem name="index" text="All Machines" to="/" icon={<DashboardIcon />} />
 
-                <Collapse in={innerOpen} timeout="auto" unmountOnExit>
-                    <List component="nav" >
-                        {machineData.map((machine) => (
-                            <ListItem name={machine.machineID} text={"Machine #" + machine.machineID} to={"/" + machine.machineID} />
-                        ))}
-                    </List>
-                </Collapse>
+                    <ListItemButton sx={{ paddingY: "15px" }} onClick={openSubList}>
+                        <ListItemIcon><PrecisionManufacturingIcon /></ListItemIcon>
+                        <ListItemText primary="Machine Details" />
+                        {innerOpen ? <ExpandLess /> : <ExpandMore />}
+                    </ListItemButton>
+                    <Collapse in={innerOpen} timeout="auto" unmountOnExit>
+                        <List component="nav">
+                            {machineData.map((machine) => (
+                                <ListItem name={machine.machineID} text={"Machine #" + machine.machineID} to={"/" + machine.machineID} />
+                            ))}
+                        </List>
+                    </Collapse></>
+                    :
+                    <Box display={'flex'}>
+                        <IconButton sx={{ paddingY: "15px" }} onClick={handleToggle}>
+                            <MenuIcon />
+                        </IconButton>
+                    </Box>
+                }
             </List>
         </Box>
     );
