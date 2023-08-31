561b8aff31f69617822d45a645a587af10154144
janenotjung-hue <107402425+janenotjung-hue@users.noreply.github.com>
2023-08-31 13:10:48 +1200

    Updated Sidebar (anim)
    added animation

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -4,25 +4,23 @@ import { Link } from "react-router-dom";
 import { useMachineData } from '../data/FireBaseData';
 import MenuIcon from '@mui/icons-material/Menu';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import PrecisionManufacturingIcon from '@mui/icons-material/PrecisionManufacturing';
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import ExpandLess from '@mui/icons-material/ExpandLess';
 import ExpandMore from '@mui/icons-material/ExpandMore';
 
-export const defaultDrawerWidth = 300;
 const minDrawerWidth = 50;
-const maxDrawerWidth = 300;
+const maxDrawerWidth = 250;
 
 const Sidebar = () => {
     const { machineData, error } = useMachineData();
     const [selected, setSelected] = useState("index");
     const [innerOpen, setInnerOpen] = useState(true);
     const theme = useTheme().palette;
-    const [drawerWidth, setDrawerWidth] = React.useState(defaultDrawerWidth);
+    const [isClosed, setIsCollapsed] = React.useState(false);
 
     const handleToggle = () => {
-        (drawerWidth === 300 ? setDrawerWidth(minDrawerWidth) : setDrawerWidth(maxDrawerWidth));
+        setIsCollapsed((prev) => !prev);
     };
 
     const openSubList = () => {
@@ -48,36 +46,41 @@ const Sidebar = () => {
     };
 
     return (
-        <Box width={drawerWidth} backgroundColor={(theme.mode === "dark" ? '#121212' : "#fff")}>
-            <List component="nav" sx={{ paddingX: "5px" }}>
-                {drawerWidth === 300 ? <>
-                    <Box display={'flex'} flexDirection={'row-reverse'}>
-                        <IconButton sx={{ alignSelf: "right", paddingY: "15px"}} onClick={handleToggle}>
-                            <ChevronLeftIcon />
-                        </IconButton>
-                    </Box>
-                    <ListItem name="index" text="All Machines" to="/" icon={<DashboardIcon />} />
+        <Box backgroundColor={(theme.mode === "dark" ? '#121212' : "#fff")}>
+            <Collapse orientation={"horizontal"} in={isClosed} collapsedSize={minDrawerWidth} timeout={300} >
+                <Box  width={maxDrawerWidth}>
+                <List component="nav" sx={{ paddingX: "5px" }}>
+                    {isClosed === true ? <>
+                        <Box display={'flex'} flexDirection={'row-reverse'}>
+                            <IconButton sx={{ alignSelf: "right", paddingY: "15px" }} onClick={handleToggle}>
+                                <ChevronLeftIcon />
+                            </IconButton>
+                        </Box>
+                        <ListItem name="index" text="All Machines" to="/" icon={<DashboardIcon />} />
 
-                    <ListItemButton sx={{ paddingY: "15px" }} onClick={openSubList}>
-                        <ListItemIcon><PrecisionManufacturingIcon /></ListItemIcon>
-                        <ListItemText primary="Machine Details" />
-                        {innerOpen ? <ExpandLess /> : <ExpandMore />}
-                    </ListItemButton>
-                    <Collapse in={innerOpen} timeout="auto" unmountOnExit>
-                        <List component="nav">
-                            {machineData.map((machine) => (
-                                <ListItem name={machine.machineID} text={"Machine #" + machine.machineID} to={"/" + machine.machineID} />
-                            ))}
-                        </List>
-                    </Collapse></>
-                    :
-                    <Box display={'flex'}>
-                        <IconButton sx={{ paddingY: "15px" }} onClick={handleToggle}>
-                            <MenuIcon />
-                        </IconButton>
-                    </Box>
-                }
-            </List>
+                        <ListItemButton sx={{ paddingY: "15px" }} onClick={openSubList}>
+                            <ListItemIcon><PrecisionManufacturingIcon /></ListItemIcon>
+                            <ListItemText primary="Machine Details" />
+                            {innerOpen ? <ExpandLess /> : <ExpandMore />}
+                        </ListItemButton>
+                        <Collapse in={innerOpen} timeout="auto" unmountOnExit>
+                            <List component="nav">
+                                {machineData.map((machine) => (
+                                    <ListItem name={machine.machineID} text={"Machine #" + machine.machineID} to={"/" + machine.machineID} />
+                                ))}
+                            </List>
+                        </Collapse></>
+                        :
+                        <Box display={'flex'}>
+                            <IconButton sx={{ paddingY: "15px" }} onClick={handleToggle}>
+                                <MenuIcon />
+                            </IconButton>
+                        </Box>
+                    }
+                </List>
+            
+                </Box>
+                </Collapse>
         </Box>
     );
 };
